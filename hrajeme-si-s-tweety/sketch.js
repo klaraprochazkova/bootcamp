8d48a2c6bf65890184a64137e2e6a80d00a3202f
klara.prochazkova <klara.prochazkova@skaut.cz>
2018-06-30 11:15:54 +0200

    Ptáčátko

---
+++ hrajeme-si-s-tweety/sketch.js
@@ -0,0 +1,13 @@
+// do not remove, allowes autocomplete
+/// <reference path="./../p5.global-mode.d.ts" />
+
+function setup() {
+  // put setup code here
+  var vysledek = tweets[0].text.replace("rep", "hip hop")
+  console.log(vysledek);
+
+}
+
+function draw() {
+  // put drawing code here
+}
--- rothko/empty-example/sketch.js
+++ rothko/empty-example/sketch.js
@@ -1,15 +1,63 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
 
+var meals = ["🍗", "🥩", "🥓", "🌮", "🍱", "🍞", "🍦"];
+var currentMeal = null;
+var currentMealPos = 0;
+var saturated = 100;
+var time = 0;
+
 function setup() {
-  createCanvas(400, 400);
-
-  fill ("red");
-
-  rect(10, 10, 100, 100);
-  // put setup code here
+  createCanvas(800, 400);
+  colorMode(HSB, 100);
+  angleMode(DEGREES)
 }
 
 function draw() {
-  // put drawing code here
+  background("green");
+
+  if (currentMeal == null) {
+    currentMeal = meals[Math.floor(Math.random()*meals.length)];
+    currentMealPos = 0;
+  } else {
+    if (currentMealPos > 300) {
+      currentMeal = meals[Math.floor(Math.random()*meals.length)];
+      currentMealPos = 0;
+      saturated = saturated + 10;
+    } else {
+      currentMealPos = currentMealPos + 5;
+    }
+  }
+
+
+
+  textSize(120);
+  textAlign(CENTER);
+
+
+  text(currentMeal, 400, currentMealPos);
+  textSize(saturated + 20);
+
+  fill("yellow");
+  ellipse(400, 300, saturated);
+  fill("blue")
+  ellipse(380, 300, 20, 20);
+
+  fill("red");
+  
+  arc(400, 300, saturated, saturated,-90 - 20 + Math.sin(time)*20, -90 + 20 - Math.sin(time)*20);
+
+  time = time+1
+
+
+
+  //fill("")
+  //arc(400, 300, 100, 100, PI, PI + QUARTER_PI);
+
+
+
+  
+
+  fill("blue");
+  rect(0, 350, 800, 350);
 }
